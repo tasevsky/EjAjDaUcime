45de5e0cbf3a0315bc188a9942d71832b06dc4db
Milan Tasevski <tasevski.milan22@hotmail.com>
2022-08-24 13:11:45 +0200

    init app and add login template

---
+++ src/main/kotlin/mk/ukim/finki/ejajdaucime/EjAjDaUcimeApplication.kt
@@ -0,0 +1,11 @@
+package mk.ukim.finki.ejajdaucime
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EjAjDaUcimeApplication
+
+fun main(args: Array<String>) {
+	runApplication<EjAjDaUcimeApplication>(*args)
+}
