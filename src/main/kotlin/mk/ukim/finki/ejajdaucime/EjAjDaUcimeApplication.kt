076bbd0d8c6b323ddb20bba450e0a13a58d74cde
Milan Tasevski <tasevski.milan22@hotmail.com>
2022-08-25 21:30:39 +0200

    fix database connection issue

--- src/main/kotlin/mk/ukim/finki/ejajdaucime/EjAjDaUcimeApplication.kt
+++ src/main/kotlin/mk/ukim/finki/ejajdaucime/EjAjDaUcimeApplication.kt
@@ -1,6 +1,7 @@
 package mk.ukim.finki.ejajdaucime
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
